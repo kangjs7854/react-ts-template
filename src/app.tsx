c665cc03f5b0280274531a5f07dd5e0ae3d1b393
kangjs7854 <kangjs7854@163.com>
2020-08-19 19:02:06 +0800

    引入antd

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,18 @@
 /*
  * @Date: 2020-08-19 10:57:26
  * @LastEditors: kjs
- * @LastEditTime: 2020-08-19 18:35:07
+ * @LastEditTime: 2020-08-19 19:00:21
  * @FilePath: \react-ts-template\src\app.tsx
 //  */
-import React from 'react'
+import React, { FC } from 'react';
+import FormSizeDemo from './components/form'
 import './app.scss'
-import Header from '@/components/Header'
 
-interface IProps {
-  name: string
-  age: number
-}
-
-function App(props: IProps) {
-  const { name, age,sex } = props
-  return (
-    <div className='app'>
-      <Header />
-      <span>{`Hello! ssI'm ${name},sssssss ${age} years old.`}</span>
-    </div>
-  )
-}
+const App: FC = () => (
+  <div className="App">
+    <FormSizeDemo />
+  </div>
+);
 
 export default App
 
